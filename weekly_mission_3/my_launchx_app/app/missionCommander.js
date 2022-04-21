390e34820447f0d86656e74f076a921ce2a54cc5
OscarLedezma <ozkr@live.com.mx>
2022-04-20 22:30:03 -0500

    Subiendo Practica 1

---
+++ weekly_mission_3/my_launchx_app/app/missionCommander.js
@@ -0,0 +1,11 @@
+class MissionCommander {
+    constructor (name) {
+      this.name = name
+    }
+  }
+  
+  // Esta línea nos permite exportar nuestra clase
+  module.exports = MissionCommander
+
+
+  
---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const oscar = new MissionCommander("Oscar")
+console.log(carlo.name)
+
+const mayda = new MissionCommander("Mayda")
+console.log(carlo.name)
